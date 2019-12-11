40e4b987b1e173e0ba7a4a37aa9d0da878efad19
boden1080 <45050369+boden1080@users.noreply.github.com>
2019-12-11 10:22:58 +0000

    Create meeting.h

---
+++ meeting.h
@@ -0,0 +1,17 @@
+#pragma once
+class Meeting
+{
+public:
+	//constructors
+	Meeting(int time, int date);
+	~Meeting();
+
+public:
+	//attributes
+	bool projector;
+	int attendence, time, date;
+
+private:
+
+
+};
