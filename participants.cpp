b16a120ea1024e888d9ea4efed9ffd07f278032d
boden1080 <45050369+boden1080@users.noreply.github.com>
2019-12-11 10:22:38 +0000

    Create participants.cpp

---
+++ participants.cpp
@@ -0,0 +1,24 @@
+#include "Participants.h"
+using namespace std;
+
+
+Participants::Participants(string name) : name(name)
+{
+}
+
+
+Participants::~Participants()
+{
+}
+void Participants::addPreferenceDate(int date)
+{
+
+}
+void Participants::addExclusionDate(int date)
+{
+
+}
+string Participants::getName() const
+{
+	return name;
+}
